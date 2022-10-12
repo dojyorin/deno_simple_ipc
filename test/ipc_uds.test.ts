7c7c8e141f0363209cf3065830d862aefb648eec
Kazuki Ota <ota_droid@live.jp>
2022-10-12 17:06:54 +0900

    test request.

--- test/ipc_uds.test.ts
+++ test/ipc_uds.test.ts
@@ -4,6 +4,10 @@ import {ipcListen, ipcRequest, ipcBroadcast} from "../src/ipc_uds.ts";
 const ch1 = "test_ch1";
 const ch2 = "test_ch2";
 
+function isAlive(rid:number){
+    return Deno.resources()[rid] === "unixListener";
+}
+
 Deno.test({
     name: "Listen and Broadcast.",
     async fn(){
@@ -14,24 +18,26 @@ Deno.test({
 
         await ipcBroadcast(ch1, "request");
 
-        while(Deno.resources()[ipc.rid] === "unixListener"){
+        while(isAlive(ipc.rid)){
             await delay(100);
         }
     }
 });
 
-// Deno.test({
-//     name: "Listen and Request.",
-//     async fn(){
-//         const ipc = ipcListen(ch2, (data:string)=>{
-//             assertEquals(data, "request");
+Deno.test({
+    name: "Listen and Request.",
+    async fn(){
+        const ipc = ipcListen(ch2, (data:string)=>{
+            assertEquals(data, "request");
 
-//             return "response";
-//         });
+            return "response";
+        });
 
-//         const response = await ipcRequest<string, string>(ch2, "request");
-//         assertEquals(response, "response");
+        const response = await ipcRequest<string, string>(ch2, "request");
+        assertEquals(response, "response");
 
-//         ipc.close();
-//     }
-// });
+        while(isAlive(ipc.rid)){
+            await delay(100);
+        }
+    }
+});
