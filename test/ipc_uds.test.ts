fd780f9a0dce7cbf2301bebf7964b1a2a541575b
Kazuki Ota <ota_droid@live.jp>
2022-10-13 18:44:26 +0900

    skip windows comment.

--- test/ipc_uds.test.ts
+++ test/ipc_uds.test.ts
@@ -1,17 +1,15 @@
 import {assertEquals, delay} from "../deps.test.ts";
 import {listenUdsRequest, listenUdsBroadcast, postUdsRequest, postUdsBroadcast} from "../src/ipc_uds.ts";
 
-// << No Windows Support >>
-// This part will be removed if deno supports unix socket on windows.
-// Reference: https://github.com/tokio-rs/mio/pull/1610
-const osWin = Deno.build.os === "windows";
-
 const ch1 = "ch0";
 const ch2 = "ch1";
 
 Deno.test({
+    // << No Windows Support >>
+    // This part will be removed if deno supports unix socket on windows.
+    // Reference: https://github.com/tokio-rs/mio/pull/1610
+    ignore: Deno.build.os === "windows",
     name: "UDS: Listen and Broadcast.",
-    ignore: osWin,
     async fn(){
         const ipc = listenUdsBroadcast(ch1, (data:string)=>{
             assertEquals(data, "request");
@@ -28,8 +26,11 @@ Deno.test({
 });
 
 Deno.test({
+    // << No Windows Support >>
+    // This part will be removed if deno supports unix socket on windows.
+    // Reference: https://github.com/tokio-rs/mio/pull/1610
+    ignore: Deno.build.os === "windows",
     name: "UDS: Listen and Request.",
-    ignore: osWin,
     async fn(){
         const ipc = listenUdsRequest(ch2, (data:string)=>{
             assertEquals(data, "request");
