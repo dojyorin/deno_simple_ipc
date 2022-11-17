3d2aa6ea16e40bc65c3b6783a0cd77d514086897
Kazuki Ota <ota_droid@live.jp>
2022-11-17 19:23:13 +0900

    update deps.

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export {type JsonValue} from "https://deno.land/std@0.160.0/encoding/json/stream.ts";
-export {type VarnumOptions, readVarnum, writeVarnum} from "https://deno.land/std@0.160.0/encoding/binary.ts";
-export {readAll, writeAll} from "https://deno.land/std@0.160.0/streams/mod.ts";
+export {type JsonValue} from "https://deno.land/std@0.165.0/encoding/json/stream.ts";
+export {type VarnumOptions, readVarnum, writeVarnum} from "https://deno.land/std@0.165.0/encoding/binary.ts";
+export {readAll, writeAll} from "https://deno.land/std@0.165.0/streams/mod.ts";
