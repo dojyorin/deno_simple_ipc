09b830215bc0f01a86d4d2f3f797e53b35324372
Kazuki Ota <ota_droid@live.jp>
2022-11-29 02:09:00 +0900

    update deps.

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export {type VarnumOptions, readVarnum, writeVarnum} from "https://deno.land/std@0.166.0/encoding/binary.ts";
 export {readAll, writeAll} from "https://deno.land/std@0.166.0/streams/mod.ts";
 
-export {type JsonStruct, ucEncode, ucDecode, tmpPath, isWin} from "https://deno.land/x/simple_utility@v0.0.7/mod.ts";
+export {type JsonStruct, ucEncode, ucDecode, tmpPath, isWin} from "https://deno.land/x/simple_utility@v0.0.8/mod.ts";
