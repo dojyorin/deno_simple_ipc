c4f31a19079981db3b55a4da69c9d81479ffb145
Kazuki Ota <ota_droid@live.jp>
2022-12-20 19:48:39 +0900

    update deps.

--- deps.test.ts
+++ deps.test.ts
@@ -1,4 +1,4 @@
-export {assertEquals} from "https://deno.land/std@0.167.0/testing/asserts.ts";
-export {delay} from "https://deno.land/std@0.167.0/async/mod.ts";
+export {assertEquals} from "https://deno.land/std@0.170.0/testing/asserts.ts";
+export {delay} from "https://deno.land/std@0.170.0/async/mod.ts";
 
-export {isWin} from "https://deno.land/x/simple_utility@v0.0.12/mod.ts";
+export {isWin} from "https://deno.land/x/simple_utility@v0.0.13/mod.ts";
